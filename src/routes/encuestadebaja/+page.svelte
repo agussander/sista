c1efabe7815866ad79fe797aa09830735a75640f
agus <agustinsander@gmail.com>
2026-07-03 11:03:40 -0300

    feat: agregar ruta /encuestadebaja

---
+++ src/routes/encuestadebaja/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+import { MetaTags } from 'svelte-meta-tags';
+import EncuestaBaja from './_components/EncuestaBaja.svelte';
+</script>
+
+<MetaTags
+	title="Sista - Encuesta de baja"
+	description="Contanos por qué diste de baja el servicio para que podamos mejorar."
+></MetaTags>
+
+<EncuestaBaja></EncuestaBaja>
