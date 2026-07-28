e82b5c218f4f98b16226e2478f698997eade3854
agus <agustinsander@gmail.com>
2026-07-28 10:45:19 -0300

    dev: proxy de lineavip e internacional en el server de vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/lineavip': {
+				target: 'https://sista.com.ar',
+				changeOrigin: true,
+				secure: true
+			},
+			'/internacional': {
+				target: 'https://sista.com.ar',
+				changeOrigin: true,
+				secure: true
+			}
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		environment: 'node'
